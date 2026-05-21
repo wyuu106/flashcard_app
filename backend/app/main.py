d58e33dea12942b4d9ab74c1af0e91048d0f9b24
渡部 <wyuu1006@gmail.com>
2026-05-21 23:19:38 +0900

    デプロイ用に修正

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.routers import user_router, card_router, quiz_router
 from app.db import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://xxxxx.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 app.include_router(card_router.router)
