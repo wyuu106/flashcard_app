bd55ab61fd13c3e9db5094c0d00d85419a7c5b75
渡部 <wyuu1006@gmail.com>
2026-05-21 23:36:23 +0900

    デプロイ用修正

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://xxxxx.vercel.app"
+        "https://flashcard-app-ecru-six.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
