a158e4e45bc7664909a4bbdef51e47b077e3294c
渡部 <wyuu1006@gmail.com>
2026-05-21 21:47:49 +0900

    必要exp修正

--- backend/app/utils/level_util.py
+++ backend/app/utils/level_util.py
@@ -3,18 +3,33 @@ from app.models.user_model import User
 MAX_LEVEL = 100
 
 def required_exp(level):
-    if level < 60:
-        return level * 100
+    if level < 20:
+        return level * 30
     
-    if 60 <= level:
-        return 2 ** (level // 6)
+    elif 20 <= level < 40:
+        return level * 50
+    
+    elif 40 <= level < 60:
+        return level * 100
+
+    elif 60 <= level < 80:
+        return level * 150
+
+    elif 80 <= level < 90:
+        return level * 200
+    
+    elif 90 <= level < 95:
+        return level * 300
+    
+    elif 95 <= level < 99:
+        return level * 500
 
 def level_up(user: User, get_exp: int) -> User:
     level = user.level
     exp = user.exp + get_exp
 
     while level <= MAX_LEVEL:
-        need = required_exp(level)
+        need = required_exp(level) # level upに必要なexp
 
         if exp < need:
             break
@@ -22,7 +37,7 @@ def level_up(user: User, get_exp: int) -> User:
         exp -= need
         level += 1
 
-        if level == 100:
+        if level == MAX_LEVEL:
             exp = 0
 
     user.level = level
